2f85be0d56136774187dc0a3ba6c4e6702746e5e
Edith Quijano <equijanoricalde@gmail.com>
2025-06-05 19:21:38 -0600

    Modificando videos en videobundle y agregando soporte para responsividad

--- config/videobundle.php
+++ config/videobundle.php
@@ -4,19 +4,22 @@ return array (
     'tutorial' =>
         array (
             'id' => 1,
-            'url' =>  'videos/video.mp4',
+            'url_movil' =>  'videos/intro_movil.mp4',
+            'url_desktop' =>  'videos/intro_desktop.mp4',
             'type' => 'tutorial'
         ),
     'idle' =>
         array (
             'id' => 2,
-            'url' => 'videos/avatar_idle_2.mp4',
+            'url_movil' => 'videos/iddle_movil.mp4',
+            'url_desktop' =>  'videos/iddle_desktop.mp4',
             'type' => 'idle'
         ),
     'talking' =>
         array (
             'id' => 3,
-            'url' => 'videos/avatar_speaking_2.mp4',
+            'url_movil' => 'videos/talk_movil.mp4',
+            'url_desktop' =>  'videos/talk_desktop.mp4',
             'type' => 'talking'
         ),
 );
