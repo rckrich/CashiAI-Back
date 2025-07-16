de2d3c0ce22983920a8b9fbeb4ae14645877aea2
Edith Quijano <equijanoricalde@gmail.com>
2025-07-16 14:38:00 -0600

    Cambiano videos para iddle y talking en videobundle

--- config/videobundle.php
+++ config/videobundle.php
@@ -11,15 +11,15 @@ return array (
     'idle' =>
         array (
             'id' => 2,
-            'url_movil' => 'videos/avatar_idle_2.mp4',
-            'url_desktop' =>  'videos/avatar_idle_2.mp4',
+            'url_movil' => 'videos/Cashimiro_Idle_Mobile.mp4',
+            'url_desktop' =>  'videos/Cashimiro_Idle_PC.mp4',
             'type' => 'idle'
         ),
     'talking' =>
         array (
             'id' => 3,
-            'url_movil' => 'videos/avatar_speaking_2.mp4',
-            'url_desktop' =>  'videos/avatar_speaking_2.mp4',
+            'url_movil' => 'videos/Cashimiro_Talk_Mobile.mp4',
+            'url_desktop' =>  'videos/Cashimiro_Talk_PC.mp4',
             'type' => 'talking'
         ),
 );
